76f142205058fc7ea49526c33069a69d632dfa0d
Dahir Mohamed <dmoabdi98@gmail.com>
2022-06-06 14:38:54 +0100

    imported components into APP.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import { Header } from "./components/Header";
+import { Meals } from "./components/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Meals />
     </Fragment>
   );
 }
