7874e2417a29da5ef2892ed623c65fce540ca3a2
Dahir Mohamed <dmoabdi98@gmail.com>
2022-06-07 14:36:39 +0100

    modal boiler point component and portal done

---
+++ src/components/Cart/index.js
@@ -0,0 +1,26 @@
+import classes from "./style.module.css";
+
+export const Cart = () => {
+  const cartItems = [
+    { id: "c1", name: "Sushi", amount: 2, price: 12.99 },
+    { id: "c2", name: "Sushi", amount: 2, price: 12.99 },
+    { id: "c3", name: "Sushi", amount: 2, price: 12.99 },
+  ];
+  return (
+    <div>
+      {cartItems.map((items) => (
+        <ul className={classes["cart-items"]}>
+          <li>{items.name}</li>
+        </ul>
+      ))}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.60</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Modal/index.js
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { createPortal } from "react-dom";
+import classes from "./style.module.css";
+
+const Backdrop = () => {
+  return <div className={classes.modal} />;
+};
+
+const ModalOverlay = ({ children }) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{children} </div>
+    </div>
+  );
+};
+
+export const Modal = ({ children }) => {
+  const portalElement = document.getElementById("overlays");
+  return (
+    <Fragment>
+      {createPortal(<Backdrop />, portalElement)}
+      {createPortal(<ModalOverlay>{children}</ModalOverlay>, portalElement)}
+    </Fragment>
+  );
+};
