a033551f85221e813cceb9bfc9c6482192d51c6b
Dahir Mohamed <dmoabdi98@gmail.com>
2022-06-09 14:37:15 +0100

    added bump animation on cart with useEffect

--- src/components/CartButton/index.js
+++ src/components/CartButton/index.js
@@ -1,9 +1,12 @@
+import { useEffect, useState } from "react";
 import { useCartContext } from "../../context/CartProvider";
 import { CartIcon } from "../CartIcon";
 
 import classes from "./style.module.css";
 
 export const CartButton = ({ onClickCartHandler }) => {
+  const [btnAnimated, setBtnAnimated] = useState(false);
+
   const { items } = useCartContext();
 
   const cartAmount = items.reduce(
@@ -11,8 +14,24 @@ export const CartButton = ({ onClickCartHandler }) => {
     0
   );
 
+  const btnClasses = `${classes.button} ${btnAnimated ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnAnimated(true);
+
+    const timer = setTimeout(() => setBtnAnimated(false), 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={onClickCartHandler}>
+    <button className={btnClasses} onClick={onClickCartHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
