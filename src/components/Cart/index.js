7645fe5830a1bb355767dfc7ecdaf3137afa9bd9
Dahir Mohamed <dmoabdi98@gmail.com>
2022-06-22 16:25:09 +0100

    added checkout form and send data to firebase

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { useCartContext } from "../../context/CartProvider";
 import { CartItem } from "../CartItem";
 import { Checkout } from "../Checkout";
@@ -7,8 +7,9 @@ import classes from "./style.module.css";
 
 export const Cart = ({ onClickCartHandler }) => {
   const { items, totalAmount, addItem, removeItem } = useCartContext();
-
   const [showCheckout, setShowCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
 
   const handleShowCheckout = () => setShowCheckout((prevState) => !prevState);
 
@@ -22,39 +23,90 @@ export const Cart = ({ onClickCartHandler }) => {
     removeItem(id);
   };
 
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
+      "https://food-order-app-e7a78-default-rtdb.firebaseio.com/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderItems: items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+  };
+
+  const CardModalContent = () => {
+    return (
+      <Fragment>
+        <ul className={classes["cart-items"]}>
+          {items.map((item) => (
+            <CartItem
+              {...item}
+              key={item.id}
+              cartHandlerAdd={cartHandlerAdd.bind(null, item)}
+              cartHandlerRemove={cartHandlerRemove.bind(null, item.id)}
+            />
+          ))}
+        </ul>
+        <div className={classes.total}>
+          <span>Total Amount</span>
+          <span>{totalCartAmount}</span>
+        </div>
+        <div className={classes.actions}>
+          {!showCheckout && (
+            <button
+              className={classes["button--alt"]}
+              onClick={onClickCartHandler}
+            >
+              Close
+            </button>
+          )}
+          {items.length > 0 && !showCheckout && (
+            <button onClick={handleShowCheckout} className={classes.button}>
+              Order
+            </button>
+          )}
+        </div>
+
+        {showCheckout && (
+          <Checkout
+            handleShowCheckout={handleShowCheckout}
+            submitOrderHandler={submitOrderHandler}
+          />
+        )}
+      </Fragment>
+    );
+  };
+
+  const OrderSubmitting = () => {
+    return <h1>SENDING ORDER DATA ....</h1>;
+  };
+
+  const OrderSubmitted = () => {
+    return (
+      <Fragment>
+        <h1>Successfully Sent the Order!!!</h1>
+        <div className={classes.actions}>
+          <button
+            onClick={() => window.location.reload()}
+            className={classes.button}
+          >
+            Return to Shopping
+          </button>
+        </div>
+      </Fragment>
+    );
+  };
+
   return (
     <Modal>
-      <ul className={classes["cart-items"]}>
-        {items.map((item) => (
-          <CartItem
-            {...item}
-            key={item.id}
-            cartHandlerAdd={cartHandlerAdd.bind(null, item)}
-            cartHandlerRemove={cartHandlerRemove.bind(null, item.id)}
-          />
-        ))}
-      </ul>
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalCartAmount}</span>
-      </div>
-      <div className={classes.actions}>
-        {!showCheckout && (
-          <button
-            className={classes["button--alt"]}
-            onClick={onClickCartHandler}
-          >
-            Close
-          </button>
-        )}
-        {items.length > 0 && !showCheckout && (
-          <button onClick={handleShowCheckout} className={classes.button}>
-            Order
-          </button>
-        )}
-      </div>
-
-      {showCheckout && <Checkout handleShowCheckout={handleShowCheckout} />}
+      {!isSubmitting && !didSubmit && <CardModalContent />}
+      {isSubmitting && !didSubmit && <OrderSubmitting />}
+      {didSubmit && !isSubmitting && <OrderSubmitted />}
     </Modal>
   );
 };
--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,25 +1,55 @@
 import classes from "./style.module.css";
+import { useForm } from "react-hook-form";
 
-export const Checkout = ({ handleShowCheckout }) => {
+export const Checkout = ({ handleShowCheckout, submitOrderHandler }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    submitOrderHandler(data);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          {...register("name", { required: true })}
+          type="text"
+          id="name"
+        />
       </div>
       <div className={classes.control}>
         <label htmlFor="address">Street Address</label>
-        <input type="text" id="address" />
+        <input
+          {...register("address", { required: true })}
+          type="text"
+          id="address"
+        />
       </div>
       <div className={classes.control}>
         <label htmlFor="phoneNumber">Phone Number</label>
-        <input type="number" id="phoneNumber" />
+        <input
+          {...register("phoneNumber", { required: true })}
+          type="text"
+          id="phoneNumber"
+        />
       </div>
 
-      <button type="submit">Confirm</button>
-      <button type="button" onClick={handleShowCheckout}>
-        Cancel
-      </button>
+      {errors.name || errors.address || errors.phoneNumber ? (
+        <span>All the fields are required</span>
+      ) : (
+        ""
+      )}
+
+      <div className={classes.actions}>
+        <button type="submit">Confirm</button>
+        <button type="button" onClick={handleShowCheckout}>
+          Cancel
+        </button>
+      </div>
     </form>
   );
 };
